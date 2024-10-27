bb86785e8c6716a80637872f2655ca04475f192e
Madvin <m.parvin986@gmail.com>
2024-10-27 19:14:08 +0330

    remove auth middleware

--- routes/routes.go
+++ routes/routes.go
@@ -1,8 +1,6 @@
 package routes
 
 import (
-	middlewares "github.com/BAXF/shortener/api/middleware"
-
 	"github.com/BAXF/shortener/api/handlers"
 	"github.com/BAXF/shortener/models"
 	"github.com/gin-gonic/gin"
@@ -11,8 +9,6 @@ import (
 
 func SetupRouter(db *gorm.DB, redisClient *models.RedisClient) *gin.Engine {
 	r := gin.Default()
-
-	r.Use(middlewares.GoogleAuthMiddleware())
 
 	urlHandler := handlers.URLHanlder{DB: db, Redis: redisClient}
 	r.POST("/shorten", urlHandler.CreateURL)
